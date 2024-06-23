46c15da6006aba71f003ee2a41e587b6bd6535f9
NoratheeDear <noratee5@hotmail.com>
2024-06-23 08:53:46 +0700

    move git path

---
+++ HISTORY_pw.md
@@ -0,0 +1,6 @@
+## 0.1.1
+### Added
+- `filter_text`, `custom_sort`
+## 0.1.1
+### Immigrated
+- `filter_dict`, `reorder_dict`
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Dear Norathee",
+    description="Package that would help shorten your python code providing additional functions ready to use",
+    name="python_wizard",
+    # add rc with number to the version to indicate a release candidate	
+    version="0.1.3rc1",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[""],
+    
+
+)
