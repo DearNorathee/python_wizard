7e2edc5f6e71d0e0bf3d1a49c48c36de0d2d0da0
NoratheeDear <noratee5@hotmail.com>
2024-07-02 09:01:47 +0700

    add is_list_of_tuple

--- python_wizard/sandbox1_pw.py
+++ python_wizard/sandbox1_pw.py
@@ -1,3 +1,35 @@
+from typing import *
+
+def is_list_of_tuple(input: Any) -> bool:
+    """
+    Check if the input is a list of tuples.
+
+    Parameters
+    ----------
+    input : Any
+        The object to be checked.
+
+    Returns
+    -------
+    bool
+        True if the input is a non-empty list where all elements are tuples,
+        False otherwise.
+
+    Examples
+    --------
+    >>> is_list_of_tuple([(1, 2), (3, 4)])
+    True
+    >>> is_list_of_tuple([1, 2, 3])
+    False
+    >>> is_list_of_tuple("not a list")
+    False
+    >>> is_list_of_tuple([])
+    False
+    """
+    if not isinstance(input, list) or not input:
+        return False
+    return all(isinstance(item, tuple) for item in input)
+
 def contain_all_items(my_list, items_to_check) -> bool:
     """
     Check if a list contains all items from another list.
