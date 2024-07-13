eb214270f4bb471e3521806780679a322ffd0dfe
NoratheeDear <noratee5@hotmail.com>
2024-07-13 08:45:26 +0700

    move sandbox func to pw_list

--- python_wizard/pw_list.py
+++ python_wizard/pw_list.py
@@ -1,5 +1,5 @@
 from typing import List, Literal, Union, Any, Tuple
-
+from typing import *
 
 def flatten(list_of_lists):
     # imported from "C:\Users\Heng2020\OneDrive\Python NLP\NLP 08_VocabList\VocatList_func01.py"
@@ -37,3 +37,60 @@ def filter_text(input_list:List[str],start_with = "",end_with ="", contain = "",
             out_list = [x for x in input_list if contain.lower() in x.lower()]
     
     return out_list
+
+
+def is_list_of_tuple(input: Any) -> bool:
+    """
+    Check if the input is a list of tuples.
+
+    Parameters
+    ----------
+    input : Any
+        The object to be checked.
+
+    Returns
+    -------
+    bool
+        True if the input is a non-empty list where all elements are tuples,
+        False otherwise.
+
+    Examples
+    --------
+    >>> is_list_of_tuple([(1, 2), (3, 4)])
+    True
+    >>> is_list_of_tuple([1, 2, 3])
+    False
+    >>> is_list_of_tuple("not a list")
+    False
+    >>> is_list_of_tuple([])
+    False
+    """
+    if not isinstance(input, list) or not input:
+        return False
+    return all(isinstance(item, tuple) for item in input)
+
+def contain_all_items(my_list, items_to_check) -> bool:
+    """
+    Check if a list contains all items from another list.
+
+    Args:
+        my_list (list): The list to check.
+        items_to_check (list): The list of items to check for.
+
+    Returns:
+        bool: True if my_list contains all items from items_to_check, False otherwise.
+    """
+    return all(item in my_list for item in items_to_check)
+
+def contain_any_items(my_list, items_to_check) -> bool:
+    """
+    Check if a list contains all items from another list.
+
+    Args:
+        my_list (list): The list to check.
+        items_to_check (list): The list of items to check for.
+
+    Returns:
+        bool: True if my_list contains all items from items_to_check, False otherwise.
+    """
+    return any(item in my_list for item in items_to_check)
